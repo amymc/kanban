ba1bd166744eb94fe4a795c5af4a0a7305c958f3
amymc <amy.mccarthy2@mail.dcu.ie>
2019-03-03 23:19:40 +0000

    Updates

---
+++ precache-manifest.03f9c9a6f22e996a6bd4097c3adfe072.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1e6a82acb5de693afb1b",
+    "url": "/kanban/static/js/runtime~main.1e6a82ac.js"
+  },
+  {
+    "revision": "7c382e5541768d12b6db",
+    "url": "/kanban/static/js/main.7c382e55.chunk.js"
+  },
+  {
+    "revision": "aa7a71caa9ada4739a99",
+    "url": "/kanban/static/js/2.aa7a71ca.chunk.js"
+  },
+  {
+    "revision": "7c382e5541768d12b6db",
+    "url": "/kanban/static/css/main.d3f9b8e4.chunk.css"
+  },
+  {
+    "revision": "c3d78eddb70bb9485f67e0b7dba22dd9",
+    "url": "/kanban/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6eb0108ecb4c5e72171cb64b78937f4f.js"
+  "/kanban/precache-manifest.03f9c9a6f22e996a6bd4097c3adfe072.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/kanban/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
